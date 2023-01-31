3970174a1ad9bfc179fd506aafb1b2bafac39eb8
RAfi123-cmd <rafiulhuda27@gmail.com>
2023-01-31 14:55:53 +0700

    menambahkan menu login dan register

--- app/src/main/java/com/gdscug/mooflix/di/Injection.kt
+++ app/src/main/java/com/gdscug/mooflix/di/Injection.kt
@@ -1,12 +1,18 @@
 package com.gdscug.mooflix.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.gdscug.mooflix.data.MovieRepository
 import com.gdscug.mooflix.data.remote.response.RemoteDataSource
 
+val Context.datastore: DataStore<Preferences> by preferencesDataStore("movie")
+
 object Injection {
 
-    fun providerRepository(): MovieRepository {
+    fun providerRepository(context: Context): MovieRepository {
         val remoteDataSource = RemoteDataSource.getInstance()
-        return MovieRepository.getInstance(remoteDataSource)
+        return MovieRepository.getInstance(remoteDataSource, context.datastore)
     }
 }
